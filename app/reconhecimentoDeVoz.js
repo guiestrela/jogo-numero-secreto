69d62aa5f36644f65b7db4d499d70a036e7d95f5
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-09-05 23:38:28 -0300

    Update reconhecimentoDeVoz.js

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -2,3 +2,6 @@ window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
