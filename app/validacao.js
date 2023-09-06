fcb76f730b6340b8b264273448a92ad0bf5d12bc
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-09-06 00:49:24 -0300

    finalizado

--- app/validacao.js
+++ app/validacao.js
@@ -3,18 +3,30 @@ function verificaValorValido(chute) {
 
     if (chuteForInvalido(numero)){
         elementoChute.innerHTML += '<div>Valor invalido</div>'
+        return
     }
 
     if (numeroForMaiorOuMenor(numero)) {
         elementoChute.innerHTML += `
         <div>Valor invalido: o numero secreto precisa ser entre ${menorValor} e ${maiorValor}</div>
         `
+        return
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!!</h2>
             <h3> O número secreto era ${numeroSecreto}</h3>
+
+            <Button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número screto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número screto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
         `
     }
 }
@@ -26,3 +38,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenor(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', evento => {
+    if (evento.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+} )
